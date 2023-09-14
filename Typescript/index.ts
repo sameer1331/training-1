e073bc3672f58745d3ae957f1040bda4afa22f5e
sameer1331 <mohideensameer786@gmail.com>
2023-09-14 12:30:07 +0530

    Typescript practice

---
+++ Typescript/index.ts
@@ -0,0 +1,49 @@
+/*class A{
+    i=1;
+    b=10;
+    constructor(){
+
+    }Incrmnt(){
+        return this.i++;
+    }
+}let ob= new A()
+console.log(ob.Incrmnt);
+console.log(ob.i);*/
+
+/*var n1:number;//type-Annotation
+var n2:number=2;//type-Annotation
+var n3=5;//type-inference
+var n4 = n3+100;
+var str1=n1+'string';
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(str1);*/
+
+ /*  let a: {
+    name:string;
+    age:number;
+}
+a={
+    name:'sameer';
+    age:24;
+}
+console.log(a);  */
+
+ /*  let a={
+      name:'sameer';
+      age :24;
+   }
+   console.log(a);  */
+
+     var numArr:number[]=[1,2,3,4,5];
+     var objArr:{name:string,age:number}[]=[{name:'sameer',age:24},{name:'jeeva',age:19}];
+       
+     console.log(numArr);  
+     console.log(objArr);  
+     numArr.push(7);  
+     console.log(numArr);  
+     console.log(objArr[1].name);
+     
